f26cad448bf93295210342fc9a0bc6d912ca2ccd
Your Name <you@example.com>
2025-04-03 16:04:05 -0300

    constantes

---
+++ Constantes.java
@@ -0,0 +1,18 @@
+public class Constantes {
+  public static void main(String[] args) {
+    // variavel armazena temporariamente uma informacao, podendo ter alteracao
+    String nome = "Michael";
+
+    nome = "Thor";
+
+    System.out.println(nome);
+
+    // Constates armezena valor que nao pode ser alterado
+
+    final String nome1 = "Michael";
+    System.out.println(nome1);
+
+    // nome1 = "Thor";
+
+  }
+}
