d23d9046fdf794e6723918ad576e5067535525da
Your Name <you@example.com>
2025-04-05 10:57:54 -0300

    condicional e operador ternario

---
+++ Condicional.java
@@ -0,0 +1,22 @@
+public class Condicional {
+public static void main(String[] args) {
+
+  // Variavel
+  double nota = 3;
+
+  // condicional
+
+  if(nota >= 7)
+  {
+    System.out.println("Aprovado(a)");
+  } 
+  else if(nota >=5){
+    System.out.println("Em recuperacao");
+  }
+  else{
+    System.out.println("Reprovado(a)");
+  }
+  
+}
+  
+}
---
+++ OperadorTernario.java
@@ -0,0 +1,14 @@
+public class OperadorTernario {
+  public static void main(String[] args) {
+    // variavel
+    double nota = 8;
+
+    //operador ternario
+    System.out.println( nota >= 7 ? "Aprovado(a)" : "Reprovado(a)");
+    
+    String situacao = nota  <= 5 ? "Aprovado(a)" : "Reprovado(a)";
+
+    System.out.println(situacao);
+  
+  }
+}
